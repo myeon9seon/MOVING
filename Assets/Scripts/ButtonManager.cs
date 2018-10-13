0a4f769fee5bc10f24064925ba62c39d2281a086
myeon9seon <myeon9seon@gmail.com>
2018-10-13 14:00:37 +0900

    Create project

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+
+    public GameObject Ribbon;
+
+    public void OnRibbonMenu()
+    {
+        Ribbon.SetActive(!Ribbon.activeSelf);
+    }
+}
---
+++ Assets/Scripts/StageManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour {
+
+    private const int stageCount = 2;
+    //public enum Stage { Circle, Rectangle };
+
+    public GameObject circle;
+    public GameObject rectangle;
+
+    private GameObject[] stages = new GameObject[stageCount];
+
+    public void Start()
+    {
+        stages[0] = circle;
+        stages[1] = rectangle;
+    }
+
+	public void OnCircleStage()
+    {
+        stages[0].SetActive(true);
+        OffOtherStage(0);
+    }
+
+    public void OnRectangleStage()
+    {
+        stages[1].SetActive(true);
+        OffOtherStage(1);
+    }
+
+    private void OffOtherStage(int curStage)
+    {
+        for (int i = 0; i < stageCount; i++)
+        {
+            if (i == curStage)
+                continue;
+            else
+                stages[i].SetActive(false);
+        }
+    }
+}
